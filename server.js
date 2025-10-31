a27ce795da4f9a838ea36c5177d55ed0b12c4868
Aradhya <2k23.cs2312107@gmail.com>
2025-10-31 16:40:17 +0530

    fix: removed clerk based routes

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config';
-import { clerkMiddleware, requireAuth } from '@clerk/express'
 import aiRouter from "./routes/aiRoutes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(clerkMiddleware());
 
 app.get('/', (req, res) => res.send("Server is running"));
 
