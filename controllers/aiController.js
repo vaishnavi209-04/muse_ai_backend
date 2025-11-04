05d7a69b97ef2485e98e29b56523c7b617df63f1
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2025-11-04 18:23:02 +0530

    feat:implemented sever side rendering

--- controllers/aiController.js
+++ controllers/aiController.js
@@ -151,7 +151,7 @@ export const removeImageBackground= async(req, res) => {
     try{
 
         const {userId}= req.auth();     
-        const {image}= req.file;
+        const image= req.file;
         const plan = req.plan
     
         if(plan !== 'premium' ){
